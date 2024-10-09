45a8fa973768d05a5b5d776d4b2cd7b9b0562ebb
Ann Yan <83194460+anne-yyyy@users.noreply.github.com>
2024-10-09 12:13:18 +1100

    Add files via upload

--- Couch Tracking/Sender/python/UDPsend_MultiROI.py
+++ Couch Tracking/Sender/python/UDPsend_MultiROI.py
@@ -29,6 +29,20 @@ class UDP_Communication:
         #byteArray += b'\x00' * (9 - len(byteArray))
  
         return byteArray
+    
+    def convert_distance_to_bytes(self, distance):
+        value = distance*100
+        # Convert positional information to 32-bit float
+        val = struct.pack('<f', value) # Little-endian byte order
+        
+        # Construct the byte array to send over UDP
+        byteArray = b't' + val
+ 
+        # Pad the byte array with zeros to make it 9 bytes long
+        #byteArray += b'\x00' * (9 - len(byteArray))
+ 
+        return byteArray
+    '''
     def convert_count_to_bytes(self, count):
         # Convert count value to 32-bit unsigned integer (UInt32 in C#)
         val = struct.pack('<I', count)  # Little-endian byte order
@@ -40,11 +54,18 @@ class UDP_Communication:
         #byteArray += b'\x00' * (6 - len(byteArray))
  
         return byteArray
+    '''
+    def convert_count_to_bytes(self, count):
+       
+       # Convert count value to 32-bit float 
+       val = struct.pack('<f', count)  # Little-endian byte order
+       byteArray = b'c' + val
+       return byteArray
  
     def send_to_udp(self, count, x):
-        formatted_distance = "{:.3f}".format(x)
-        message = f"Count = {count}, x = {formatted_distance}"
-        byte_depth = self.convert_depth_to_bytes(x)
+        #formatted_distance = "{:.3f}".format(x)
+        message = f"Count = {count}, x = {x}"
+        byte_depth = self.convert_distance_to_bytes(x)
         byte_count = self.convert_count_to_bytes(count)
         self.sock.sendto(byte_count + byte_depth, (self.ip, self.port))
         #print(byte_count+byte_depth)
@@ -90,37 +111,44 @@ cv2.setMouseCallback("Color frame", show_distance)
 # Initialize variables
 roi = ()  # ROI coordinates
 roi_selected = False  # Flag to indicate if ROI is selected
-start_time = time.time()
-initial_time = start_time
-#time_delay = 0.1  # 10Hz frame rate
+
 dist_array_1 = [] # Store depth measurements from ROI 1
 dist_array_2 = [] # Store depth measurements from ROI 2
 time_array = []
+time_local_array = []
 frame_array = []
 frame_number = 0
 count = 0
 
 dist_1_multi = []
 dist_2_multi = []
+start_time = time.perf_counter()
+
+# KIM time interval is 350ms
+# Decide time interval that depth measurement is averaged from
+frequency_to_mimic = 10 # desired frequncy, e.g. KIM is 10Hz
+interval = 1.0/frequency_to_mimic
 
 while True:
+    # Loop start time
+    loop_start_time = time.perf_counter()
     # Get depth frame from the depth camera
     ret, depth_frame, color_frame, timestamp = dc.get_frame()
- 
+
     # Draw rectangles for selected ROIs if two ROIs are selected
     img = color_frame.copy()
     if roi_selected:
         for i, point in enumerate(points):
-            cv2.rectangle(img, point, (point[0] + 30, point[1] + 30), (255, 0, 0), 2)
+            cv2.rectangle(img, point, (point[0] + 40, point[1] + 40), (255, 0, 0), 2)
             cv2.putText(img, f"ROI {i+1}", (point[0], point[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
  
         # Calculate depth measurements for each ROI
-        distance1 = get_average_distance((points[0][0], points[0][1], 30, 30), depth_frame)
-        distance2 = get_average_distance((points[1][0], points[1][1], 30, 30), depth_frame)
+        distance1 = get_average_distance((points[0][0], points[0][1], 40, 40), depth_frame)
+        distance2 = get_average_distance((points[1][0], points[1][1], 40, 40), depth_frame)
         count += 1
         # For Lidar 515 camera
-        distance1 = distance1/4
-        distance2 = distance2/4
+        distance1 = round(distance1/4,1)
+        distance2 = round(distance2/4,1)
 
         dist_1_multi.append(distance1)
         dist_2_multi.append(distance2)
@@ -133,26 +161,39 @@ while True:
         cv2.putText(img, f"ROI 1 Distance: {distance1:.2f} mm", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
         cv2.putText(img, f"ROI 2 Distance: {distance2:.2f} mm", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-        # Decide the frame number that depth measurement is averaged from
-        if count == 10:
-            d1 = np.mean(dist_1_multi)
-            d2 = np.mean(dist_2_multi)
-            dist_array_1.append(d1)
-            dist_array_2.append(d2)
+
+
+        elapsed_time = time.perf_counter()-start_time
+
+        if elapsed_time >= interval:
+            #d1 = np.mean(dist_1_multi)
+            #d2 = np.mean(dist_2_multi)
+            #dist_array_1.append(d1)
+            #dist_array_2.append(d2)
             frame_number += 1
             frame_array.append(frame_number)
             time_array.append(timestamp)
             # Empty the depth measurement array and later store the new measurements
-            dist_1_multi = []
-            dist_2_multi = []
-            count = 0
+            #dist_1_multi = []
+            #dist_2_multi = []
             # Send only one depth measurement over UDP (for example, send distance1)
-            udp_sender.send_to_udp(frame_number, d1)  # Convert mm to meters
-            print(d1)
+            #udp_sender.send_to_udp(frame_number, d1)  # Convert mm to meters
+            time_local_array.append(time.time())
+        
+            udp_sender.send_to_udp(frame_number, distance1)
+            dist_array_1.append(distance1)
+            dist_array_2.append(distance2)
+            print(distance1)
             #time.sleep(time_delay)
  
     # Display the depth frame
     cv2.imshow("Color frame", img)
+
+    # Calculate the time taken for the loop and sleep to maintain consistent frequency
+    loop_end_time = time.perf_counter()
+    loop_duration = loop_end_time - loop_start_time
+    if loop_duration < interval:
+        time.sleep(interval-loop_duration)
  
     key = cv2.waitKey(1)
  
@@ -161,7 +202,7 @@ while True:
         break
 
 # Save the measurements to a CSV file along with timestamps
-data_to_save = np.column_stack((frame_array, time_array, dist_array_1, dist_array_2))
-np.savetxt('1D_only_sine_2.5s_5mm.csv', data_to_save, delimiter=',', header='Count,Time,Distance 1, Distance 2', comments='')
+data_to_save = np.column_stack((frame_array, time_array, time_local_array, dist_array_1, dist_array_2))
+np.savetxt('new_lung_typical.csv', data_to_save, delimiter=',', header='Count,Time, Local Time, Distance 1, Distance 2', comments='')
 # Release resources
 cv2.destroyAllWindows()
