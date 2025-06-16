c468619855f3cc9a4fdfbb4d06940c40dc8315fc
Ann Yan <83194460+anne-yyyy@users.noreply.github.com>
2025-06-16 15:34:27 +1000

    Update realsense_depth.py

--- Couch Tracking/Target tracking/realsense_depth.py
+++ Couch Tracking/Target tracking/realsense_depth.py
@@ -16,7 +16,7 @@ class DepthCamera:
         # For L515
         config.enable_stream(rs.stream.depth, 1024, 768, rs.format.z16, 30) 
         config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
-
+        # For D415
         #config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
         #config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
